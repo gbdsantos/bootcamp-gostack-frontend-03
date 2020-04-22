01bdeb9930ec0acc77b45b7bbb9d77988729ba68
Guilherme Bezerra <gbsantos.it@gmail.com>
2020-04-21 23:40:11 -0300

    :nail_care: style(frontend): stylesheet dashboard
    - Create styled components for Dashboard page.

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi'
 
-import { Title } from './styles';
+import logoImg from '../../assets/github-logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form action="">
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars1.githubusercontent.com/u/21063343?s=460&u=31d834e21b18a3788f5532ab63ef9d6b453b1799&v=4"
+            alt="Guilherme Bezerra"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
